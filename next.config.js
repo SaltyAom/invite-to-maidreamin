4a780452ec22ee5940d4f7c4f7bd54733461e5fb
aomkirby123 <aomkirby123@gmail.com>
2019-07-29 16:03:56 +0700

    Correcting most thing

--- next.config.js
+++ next.config.js
@@ -1,17 +1,49 @@
-const withCSS = require("@zeit/next-css")
-const withFonts = require("next-fonts");
+const path = require('path')
+const withStylus = require('@zeit/next-stylus')
 
 module.exports = withCSS(
-    withFonts({
+    withStylus({
+        exportPathMap: () => {
+            return {
+                '/': {
+                    page: '/',
+                },
+            }
+        },
         target: 'serverless',
-        webpack (config, options) {
-            config.resolve.alias['react'] = 'preact/compat'
-            config.resolve.alias['react-dom'] = 'preact/compat'
-            config.resolve.alias['components'] = 'components'
-            config.resolve.alias['css'] = 'static/css'
-            config.resolve.alias['static'] = 'static'
+        webpack(config, options) {
+            ;(config.resolve.alias['react'] = 'preact/compat'),
+                (config.resolve.alias['react-dom'] = 'preact/compat'),
+                (config.resolve.alias['react-ssr-prepass'] =
+                    'preact-ssr-prepass'),
+                (config.resolve.alias['react-render-to-string'] =
+                    'preact-render-to-string'),
+                (config.resolve.alias['assets'] = path.join(
+                    __dirname,
+                    'static/assets'
+                )),
+                (config.resolve.alias['css'] = path.join(
+                    __dirname,
+                    'static/css'
+                )),
+                (config.resolve.alias['components'] = path.join(
+                    __dirname,
+                    'components'
+                )),
+                (config.resolve.alias['static'] = path.join(
+                    __dirname,
+                    'static'
+                )),
+                (config.resolve.alias['stores'] = path.join(
+                    __dirname,
+                    'stores'
+                )),
+                (config.resolve.alias['icon'] = path.join(
+                    __dirname,
+                    'components/icon'
+                ))
 
             return config
-        }
+        },
     })
-);
+)
