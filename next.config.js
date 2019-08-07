66ac310aa99d750b392b0c1b52dfda038decdb63
aomkirby123 <aomkirby123@gmail.com>
2019-08-07 18:51:30 +0700

    :game_die: Init

--- next.config.js
+++ next.config.js
@@ -1,49 +1,37 @@
-const path = require('path')
-const withStylus = require('@zeit/next-stylus')
+const path = require("path")
+const withStylus = require("@zeit/next-stylus")
 
-module.exports = withCSS(
-    withStylus({
-        exportPathMap: () => {
-            return {
-                '/': {
-                    page: '/',
-                },
-            }
-        },
-        target: 'serverless',
-        webpack(config, options) {
-            ;(config.resolve.alias['react'] = 'preact/compat'),
-                (config.resolve.alias['react-dom'] = 'preact/compat'),
-                (config.resolve.alias['react-ssr-prepass'] =
-                    'preact-ssr-prepass'),
-                (config.resolve.alias['react-render-to-string'] =
-                    'preact-render-to-string'),
-                (config.resolve.alias['assets'] = path.join(
-                    __dirname,
-                    'static/assets'
-                )),
-                (config.resolve.alias['css'] = path.join(
-                    __dirname,
-                    'static/css'
-                )),
-                (config.resolve.alias['components'] = path.join(
-                    __dirname,
-                    'components'
-                )),
-                (config.resolve.alias['static'] = path.join(
-                    __dirname,
-                    'static'
-                )),
-                (config.resolve.alias['stores'] = path.join(
-                    __dirname,
-                    'stores'
-                )),
-                (config.resolve.alias['icon'] = path.join(
-                    __dirname,
-                    'components/icon'
-                ))
+module.exports = withStylus({
+	exportPathMap: () => {
+		return {
+			"/": {
+				page: "/"
+			}
+		}
+	},
+	target: "serverless",
+	webpack(config, options) {
+		;(config.resolve.alias["react"] = "preact/compat"),
+			(config.resolve.alias["react-dom"] = "preact/compat"),
+			(config.resolve.alias["react-ssr-prepass"] = "preact-ssr-prepass"),
+			(config.resolve.alias["react-render-to-string"] =
+				"preact-render-to-string"),
+			(config.resolve.alias["assets"] = path.join(
+				__dirname,
+				"static/assets"
+			)),
+			(config.resolve.alias["css"] = path.join(__dirname, "static/css")),
+			(config.resolve.alias["components"] = path.join(
+				__dirname,
+				"components"
+			)),
+			(config.resolve.alias["static"] = path.join(__dirname, "static")),
+			(config.resolve.alias["stores"] = path.join(__dirname, "stores")),
+			(config.resolve.alias["icon"] = path.join(
+				__dirname,
+				"components/icon"
+			))
 
-            return config
-        },
-    })
-)
+		return config
+	}
+})
---
+++ static/service-worker.js
@@ -0,0 +1,48 @@
+
+let inviteToMaidreamin = 'v1';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(inviteToMaidreamin).then(cache => {
+            cache.addAll(['index.html']);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== inviteToMaidreamin) {
+                        console.log('Removing old cache.', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        // Try the network
+        fetch(event.request)
+            .then(res => {
+                return caches.open(inviteToMaidreamin).then(cache => {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                });
+            })
+            .catch(err => {
+                // Fallback to cache
+                return caches.match(event.request).then(res => {
+                    if (res === undefined) {
+                        // get and return the offline page
+                    }
+                    return res;
+                });
+            })
+    );
+});
